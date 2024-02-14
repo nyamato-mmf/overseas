b0418b387429f906b51d2f2b7437fea111e984ee
iusmmf <nyamato@mori-m-foundation.or.jp>
2024-02-14 16:59:42 +0900

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -3,23 +3,6 @@
 　Chart.js グラフ描画機能
 ---------------------------------------------------------------------------- */
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Add event listener to the tab links
-    document.querySelectorAll('.nav-link').forEach(function(tab) {
-        tab.addEventListener('click', function(event) {
-            // Hide the map and show the chart when the "Graph" tab is clicked
-            if (event.target.getAttribute('href') === '#mygraph') {
-                document.getElementById('map').style.display = 'none';
-                document.getElementById('canvas-container').style.display = 'block';
-            } else {
-                // Show the map and hide the chart for other tabs
-                document.getElementById('map').style.display = 'block';
-                document.getElementById('canvas-container').style.display = 'none';
-            }
-        });
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     // Sample data for different graph types
     const data = {
